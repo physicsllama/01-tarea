18281c1794de431b57e010e4b04db68d61e5d58b
Nicolas Valdes <n.valdes.meller@gmail.com>
2018-09-24 17:15:56 -0300

    Comenzar informe. Calcular derivadas en P1 y graficar

--- T1P1.py
+++ T1P1.py
@@ -14,6 +14,32 @@ Estimar la derivada de la funcion -cos(x) a orden h^4.
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+def derivada_1(f, x, step):
+    g = (1 / h) * (f(x + h) - f(x))
+    return g
 
+def derivada_4(f, x, step):
+    g = (1 / (12 * h)) * (-f(x + 2 * h) + 8 * f(x + h) - 8 * f(x - h) + f(x - 2 * h))
+    return g
 
+x0 = 1.388 
+h = np.logspace(-1, -15, 15, base = 10)
 
+h = np.float64(h)
+
+derivadas_1 = - derivada_1(np.cos, x0, h)
+derivadas_4 = - derivada_4(np.cos, x0, h)
+
+#derivadas_1 = np.float64(derivadas_1)
+#derivadas_4 = np.float64(derivadas_4)
+
+dif_1 = np.fabs(np.sin(x0) - derivadas_1)
+dif_4 = np.fabs(np.sin(x0) - derivadas_4)
+
+plt.xscale('log')
+plt.yscale('log')
+plt.plot(h, dif_1, label = "$\mathcal{O}(h)$")
+plt.plot(h, dif_4, label = "$\mathcal{O}(h^4)$")
+plt.legend()
+
+plt.show()
