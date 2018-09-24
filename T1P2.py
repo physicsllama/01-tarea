662b4a42de0d8bbcb5677ee79ed93f9dc209901a
Nicolas Valdes <n.valdes.meller@gmail.com>
2018-09-24 15:23:17 -0300

    Hice grafico P2 parte 1

---
+++ T1P2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 14:57:14 2018
+
+@author: nicolasvaldes
+
+Solves P2 of Tarea 1, Metodos Numericos 2018 Semestre 2
+
+Trabajar con datos de COBE de la radiacion de fondo cosmica. 
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+x = np.loadtxt("firas_monopole_spec_v1.txt", usecols = 0)
+y = np.loadtxt("firas_monopole_spec_v1.txt", usecols = 1)
+err = np.loadtxt("firas_monopole_spec_v1.txt", usecols = 3)
+
+fig, ax = plt.subplots() 
+ax.errorbar(x, y, xerr = 0, yerr = 400* err / 1000)
+
+plt.xlabel("Frecuencia (cm$^{-1}$)")
+plt.ylabel("Espectro de Monopolo (MJy/sr)")
+
+plt.savefig("espectro_monopolo.png",dpi=400)
+plt.show()
