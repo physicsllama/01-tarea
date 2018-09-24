d65437362c0f2eb6cba4a1b96918d50e2c0160ba
Nicolas Valdes <n.valdes.meller@gmail.com>
2018-09-24 12:46:31 -0300

    Agregar comentarios y paquetes

---
+++ T1P1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 12:31:31 2018
+
+@author: nicolasvaldes
+
+Solves P1 of Tarea 1, Metodos Numericos 2018 Semestre 2
+
+Estimar la derivada de la funcion -cos(x) a orden h^4. 
+
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+
+
