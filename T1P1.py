2674e01e86c2a1eb352606c3ebe131e4236079f5
Nicolas Valdes <n.valdes.meller@gmail.com>
2018-09-26 02:05:37 -0300

    Hacer código más legible. Terminar P1 en informe.

--- T1P1.py
+++ T1P1.py
@@ -14,36 +14,59 @@ Estimar la derivada de la funcion -cos(x) a orden h^4.
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+#funciones utiles
+
 def derivada_1(f, x, step):
+    ''' toma una funcion f, calcula su derivada en x usando el metodo de orden h. step es el h.
+    '''
     g = (1 / h) * (f(x + h) - f(x))
     return g
 
 def derivada_4(f, x, step):
+    '''toma una funcion f, calcula su derivada en x usando el metodo de orden h^4. step es el h.
+    '''
     g = (1 / (12 * h)) * (-f(x + 2 * h) + 8 * f(x + h) - 8 * f(x - h) + f(x - 2 * h))
     return g
+
+#datos iniciales 
 
 x0 = 1.388 
 h = np.logspace(-1, -15, 15, base = 10)
 
+
+
+
+#IMPLEMENTACION PARA FLOAT 32
+
+#imponer tipo de float
 x0 = np.float32(x0) 
 h = np.float32(h)
 
+#definir lista con derivadas de la funcion deseada
 derivadas_1 = - derivada_1(np.cos, x0, h)
 derivadas_4 = - derivada_4(np.cos, x0, h)
 
+#definir error
 dif_1 = np.fabs(np.sin(x0) - derivadas_1)
 dif_4 = np.fabs(np.sin(x0) - derivadas_4)
 
+#hacer graficos en escala logaritmica 
 plt.xscale('log')
 plt.yscale('log')
 plt.plot(h, dif_1, label = "$\mathcal{O}(h)$")
 plt.plot(h, dif_4, label = "$\mathcal{O}(h^4)$")
 plt.legend()
 plt.xlabel("Paso $h$")
-plt.ylabel("Error")
+plt.ylabel("Error $\Delta$")
 
-plt.savefig("PlotP1_float32.png",dpi=500)  
+plt.savefig("PlotP1_float32.png",dpi=300)  
 plt.clf()
+
+
+
+
+
+#IMPLEMENTACION PARA FLOAT 64
 
 x0 = 1.388 
 h = np.logspace(-1, -15, 15, base = 10)
@@ -63,10 +86,16 @@ plt.plot(h, dif_1, label = "$\mathcal{O}(h)$")
 plt.plot(h, dif_4, label = "$\mathcal{O}(h^4)$")
 plt.legend()
 plt.xlabel("Paso $h$")
-plt.ylabel("Error")
+plt.ylabel("Error $\Delta$")
 
-plt.savefig("PlotP1_float64.png",dpi=500)  
+plt.savefig("PlotP1_float64.png",dpi=300)  
 plt.clf()
+
+
+
+
+
+#IMPLEMENTACION PARA FLOAT 128
 
 x0 = 1.388 
 h = np.logspace(-1, -15, 15, base = 10)
@@ -86,7 +115,7 @@ plt.plot(h, dif_1, label = "$\mathcal{O}(h)$")
 plt.plot(h, dif_4, label = "$\mathcal{O}(h^4)$")
 plt.legend()
 plt.xlabel("Paso $h$")
-plt.ylabel("Error")
+plt.ylabel("Error $\Delta$")
 
-plt.savefig("PlotP1_float128.png",dpi=500)  
+plt.savefig("PlotP1_float128.png",dpi=300)  
 plt.clf()
