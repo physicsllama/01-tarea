4c711d7d87c24de8a823d526ec51a8553eca9659
Nicolas Valdes <n.valdes.meller@gmail.com>
2018-09-27 23:21:19 -0300

    Trabajo en P2. Continuar informe

--- T1P2.py
+++ T1P2.py
@@ -14,6 +14,7 @@ Trabajar con datos de COBE de la radiacion de fondo cosmica.
 import numpy as np
 import matplotlib.pyplot as plt 
 from astropy import constants as const
+import scipy.integrate
 
 #CARGAR DATOS
 
@@ -21,15 +22,20 @@ X = np.loadtxt("firas_monopole_spec_v1.txt", usecols = 0)
 Y = np.loadtxt("firas_monopole_spec_v1.txt", usecols = 1)
 err = np.loadtxt("firas_monopole_spec_v1.txt", usecols = 3)
 
+X = X * 3e10
+
+np.insert(X,0,0)
+np.insert(Y,0,0)
+
 #GRAFICAR DATOS
 fig, ax = plt.subplots() 
-ax.errorbar(X * 3e10, Y, xerr = 0, yerr = 400* err / 1000)
+ax.errorbar(X, Y, xerr = 0, yerr = 400 * err / 1000)
 
 plt.xlabel("Frecuencia (s$^{-1}$)")
 plt.ylabel("Espectro de Monopolo (MJy/sr)")
 
 plt.savefig("espectro_monopolo.png",dpi=300)
-plt.show()
+#plt.clf()
 
 
 #INTEGRAL POTENCIA; metodo trapecio
@@ -37,10 +43,10 @@ def integral(f,N,x_i = 0,x_f = np.pi / 2):
     ''' integra la funcion f usando metodo del trapecio. divide el intervalo en N partes.
         la integral por defecto va desde cero a pi/2. 
     '''
-    h = 1 / N
+    h = (x_f-x_i) / N
     x_k = x_i + h 
     I = 0 
-    for k in range(N):
+    for k in range(N-1):
         I = I + h / 2 * (f(x_k) + f(x_k + h))
         x_k = x_k + h 
     valor = I
@@ -49,13 +55,11 @@ def integral(f,N,x_i = 0,x_f = np.pi / 2):
 def Integrando(x):
     return np.sin(x)**3 / (np.cos(x)**5 * (np.exp(np.tan(x))-1))
 
-Integral = integral(Integrando,50)
+Integral = integral(Integrando,100)
 
-P = 2 * const.h / (const.c ** 2) * (const.k_B / const.h) ** 4 * Integral
+P = (2 * 6.626 * 1e-34 / ((3 * 1e8) ** 2)) * ((1.38 * 1e-23) / (6.626 * 1e-34)) ** 4 * Integral
 
 print(P)
-
-print(Integral)
 
 
 #INTEGRAR ESPECTRO
@@ -64,7 +68,39 @@ def integral_2():
     I = 0
     for k in range(n-1):
         I = I + (X[k+1]-X[k])/2 * (Y[k+1]+Y[k])
-    return I 
+    return I * 1e-20 
 
 print(integral_2())
 
+
+T2 = (integral_2() / P) ** (1/4)
+
+print(T2)
+
+#GRAFICOS NUEVOS 
+T1 = 2.725
+
+def planck(T,v):
+    B = (2 * const.h * v ** 3 / const.c ** 2) * 1e20 / (np.exp(6.626 * 1e-34 * v / (1.38 * 1e-23 *T))-1)
+    return B
+
+V = np.linspace(0,7e11,100)
+
+plt.plot(V,planck(T1,V))
+plt.plot(V,planck(T2,V))
+plt.xlabel("Frecuencia (s$^{-1}$)")
+plt.ylabel("Espectro de Monopolo (MJy/sr)")
+plt.savefig("espectro2.png",dpi=300)
+plt.show()
+
+
+
+#PARTE 5, ALGORITMOS SCIPY
+Integral_scipy = scipy.integrate.quad(Integrando,0,np.pi/2)[0]
+Integral_2_scipy = scipy.integrate.trapz(Y,X) * 1e-20
+
+R1 = Integral_scipy / Integral
+R2 = Integral_2_scipy / integral_2()
+
+print(R1)
+print(R2)
